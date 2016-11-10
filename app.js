1ef3f6100ffcf67964da1c6aafc82c10b66939b1
tsague <tsagueymele@yahoo.fr>
2016-11-10 16:50:45 +0100

    init propre

--- app.js
+++ app.js
@@ -9,27 +9,21 @@ var app ={
 
 	init : function(){
 	
-			$('input').on('change', function(){
-				this.r = $('#red').val();
-				this.g = $('#green').val();
-				this.b = $('#blue').val();
-				this.a = $('#alpha').val()/100;
-				$('span').html(this.r+ ", "+ this.g + ", " +this.b+ ", " +this.a);
-        		$('.container').css('background-color','rgba('+this.r+','+this.g+','+this.b+','+this.a+')');
+		$('input').on('change', function(){
+			this.r = $('#red').val();
+			this.g = $('#green').val();
+			this.b = $('#blue').val();
+			this.a = $('#alpha').val()/100;
+			$('span').html(this.r+ ", "+ this.g + ", " +this.b+ ", " +this.a);
+        	$('.container').css('background-color','rgba('+this.r+','+this.g+','+this.b+','+this.a+')');
         		
-			});
-			
- 
-
+		});
 	},
 };
 
 $(document).ready(function(){
-
-	console.log('hello word');
 	app.init();
 });
-
 
 })();
 
