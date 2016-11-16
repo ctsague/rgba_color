c12f64ce88e7b02d43d6cec6b6df21efc49b1ef1
tsague <tsagueymele@yahoo.fr>
2016-11-16 23:28:17 +0100

    Rgba to Haxadecimal

--- app.js
+++ app.js
@@ -14,16 +14,29 @@ var app ={
 			this.g = $('#green').val();
 			this.b = $('#blue').val();
 			this.a = $('#alpha').val()/100;
-			$('span').html(this.r+ ", "+ this.g + ", " +this.b+ ", " +this.a);
-        	$('.container').css('background-color','rgba('+this.r+','+this.g+','+this.b+','+this.a+')');
-        		
+			$('#valrgba').html("(" + this.r+ ", "+ this.g + ", " +this.b+ ", " +this.a + ")");
+        	$('body').css('background-color','rgba('+this.r+','+this.g+','+this.b+','+this.a+')');
+        	app.rgbTohexadecimal(this.r, this.g, this.b,this.a);	
 		});
+	},
+
+	rgbTohexadecimal : function(r,g,b,a){
+		
+		var rhexa = parseInt(r, 10).toString(16);
+		console.log(rhexa);
+		var ghexa = parseInt(g, 10).toString(16);
+		var bhexa = parseInt(b, 10).toString(16);
+		var ahexa = parseInt(a, 10).toString(16);
+		
+		var valhexa = ('#' + rhexa + ghexa + bhexa + ahexa).toUpperCase();
+		$('#hexa').html(valhexa);
+
 	},
 };
 
-$(document).ready(function(){
-	app.init();
-});
+	$(document).ready(function(){
+		app.init();
+	});
 
 })();
 
